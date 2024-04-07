a91694b6824a8c40c419d39bc7d175cc230b8ad0
Paras Bhardava <84320506+ParasBhardava@users.noreply.github.com>
2024-04-07 21:12:43 +0530

    Time: 0 ms (100.00%), Space: 7.3 MB (53.16%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
@@ -1,41 +1,23 @@
 class Solution {
-
-private:
-    int solve(int idx, int count, int n, string &s, vector<vector<int>>&dp)
-    {
-        if(idx == n)
-            return count == 0;
-
-        if(dp[idx][count] != -1)
-            return dp[idx][count];
-
-        bool ans = false;
-        if(s[idx] == '*')
-        {
-            ans |= solve(idx + 1, count + 1, n, s, dp);
-            if(count > 0)
-                ans |= solve(idx + 1, count - 1, n, s, dp);
-            ans |= solve(idx + 1, count, n, s, dp);
-        }
-        
-        else
-        {
-            if(s[idx] == '(')
-                ans = solve(idx + 1, count + 1, n, s, dp);
-            else
-            {
-                if(count > 0)
-                    ans = solve(idx + 1, count - 1, n, s, dp);
-            }
-        }
-
-        return dp[idx][count] = ans;
-    }
-
 public:
     bool checkValidString(string s) {
-        int n = s.size();
-        vector<vector<int>> dp(n, vector<int>(n,-1));
-        return solve(0, 0, n, s, dp);
+        int countMin = 0, countMax = 0;
+        for (char ch : s) {
+            if (ch == '(') {
+                countMax++;
+                countMin++;
+            } 
+            else if (ch == ')') {
+                countMax--;
+                countMin--;
+            } else if (ch == '*') {
+                countMax++;
+                countMin--; 
+            }
+            if (countMax < 0) 
+                return false; 
+            countMin = max(countMin, 0);
+        }
+        return countMin == 0;
     }
 };
