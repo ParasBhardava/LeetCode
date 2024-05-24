8637d65814040e73d9a760889075964573127420
Paras Bhardava <84320506+ParasBhardava@users.noreply.github.com>
2024-05-24 23:10:26 +0530

    Time: 24 ms (27.38%), Space: 30.9 MB (17.09%) - LeetHub

--- 1255-maximum-score-words-formed-by-letters/1255-maximum-score-words-formed-by-letters.cpp
+++ 1255-maximum-score-words-formed-by-letters/1255-maximum-score-words-formed-by-letters.cpp
@@ -1,43 +1,47 @@
 class Solution {
     
 private:
-    int solve(int idx, int n, vector<string>& words, vector<char>& letters, vector<int>& score, unordered_map<char, int>&mp)
+    int solve(int idx, int n, vector<string>& words, vector<char>& letters, vector<int>& score, map<char, int>&count, map<pair<int, map<char, int>>, int>&dp)
     {
         if(idx >= n)
             return 0;
+        
+        if(dp.find({idx, count}) != dp.end())
+            return dp[{idx, count}];
         
         int possibleIdx = 0, take = 0, res = 0;
         for(possibleIdx = 0; possibleIdx < words[idx].size(); possibleIdx++)
         {
             char ch = words[idx][possibleIdx];
-            if(mp[ch] == 0){
+            if(count[ch] == 0){
                 res = 0;
                 break;
             }
-            mp[ch]--;
+            count[ch]--;
             res += score[ch - 'a'];
         }
         
         if(possibleIdx == words[idx].size())
-            take = res + solve(idx + 1, n, words, letters, score, mp);
+            take = res + solve(idx + 1, n, words, letters, score, count, dp);
         
         for(int i = 0; i < possibleIdx; i++){
             char ch = words[idx][i];
-            mp[ch]++;
+            count[ch]++;
         }
         
-        int dontTake = solve(idx + 1, n, words, letters, score, mp);
-        return max(take, dontTake);
+        int dontTake = solve(idx + 1, n, words, letters, score, count, dp);
+        return dp[{idx, count}] = max(take, dontTake);
     }
     
 public:
     int maxScoreWords(vector<string>& words, vector<char>& letters, vector<int>& score) {
         
         int n = words.size();
-        unordered_map<char, int>mp;
+        map<char, int>count;
         for(char letter : letters)
-            mp[letter]++;
+            count[letter]++;
         
-        return solve(0, n, words, letters, score, mp);
+        map<pair<int, map<char, int>>, int>dp;
+        return solve(0, n, words, letters, score, count, dp);
     }
 };
